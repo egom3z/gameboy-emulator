e167c20253b83edc629eb286651548fa9d72487d
Enrique <exgc2022@gmail.com>
2025-05-24 23:00:43 -0400

    Added simple command line logic to call ROM file load method

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,30 @@
 #include "memory.hpp"
 #include "ppu.hpp"
 #include "rom.hpp"
+
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+ 
+namespace fs = std::filesystem;
 
 int main(int argc, char* argv[]) {
-  std::cout << "Initializing GameBoy emulator..." << std::endl;
+  std::cout << "Initializing GameBoy emulator...\n";
+
+  std::cout << "CWD: " << fs::current_path() << "\n";
+
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <path/to/file.gb(c)>\n";
+    return 1;
+  }
+
+  gb::ROM rom;
+  bool load_success = rom.load(argv[1]);
+  if (!load_success) {
+    return 1;
+  }
 
   while (true);
-  
+
   return 0;
 }
